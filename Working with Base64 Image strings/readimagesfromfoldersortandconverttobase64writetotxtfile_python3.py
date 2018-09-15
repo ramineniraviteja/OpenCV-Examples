f4149b174bdc65b53ba424d263642b1416670bc9
Ramineni Ravi Teja <32256153+ramineniraviteja@users.noreply.github.com>
2018-09-15 23:43:20 +0530

    Update readimagesfromfoldersortandconverttobase64writetotxtfile_python3.py

--- Working with Base64 Image strings/readimagesfromfoldersortandconverttobase64writetotxtfile_python3.py
+++ Working with Base64 Image strings/readimagesfromfoldersortandconverttobase64writetotxtfile_python3.py
@@ -3,22 +3,22 @@ import os
 import base64
 import codecs
 
+#Remove the text fileif exists
 if os.path.exists("base64.txt"):
    os.remove("base64.txt")
 else:
    print("Sorry, I can not remove file base64.txt")
 
-
+#Input Images directory
 images_dir="/home/ubuntu/input_frames"
 os.chdir(images_dir)
+#access only .jpg format images
 files = glob.glob("*.jpg")
+#Sort Filename by creation time stamp
 files.sort(key=os.path.getmtime)
-#print(files)
-#print("\n".join(files))
-#print(len(files))
 
+#Read images from input folder in a loop and convert to base64 format. Then write to Text file
 f = open(os.path.join("/home/ubuntu/", "base64.txt"), 'w')
-#with open(os.path.join("/home/ubuntu/", "base64.txt"), 'w') as f:
 i=0
 for image in files:
     i+=1
